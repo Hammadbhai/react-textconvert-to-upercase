ca5bf01694f9557395b18fff9146526751048cfd
hammad-PC\hammad <hammadansaridon@gmail.com>
2021-11-18 21:10:52 +0500

    3rd

--- src/components/Textfoam.js
+++ src/components/Textfoam.js
@@ -14,8 +14,16 @@ export default function Textfoam() {
     return (
         <div className="container-fluid">
             <h1 heading=""> Enter Text to Convert Lowercase to Upercase</h1>
-            <textarea className="form-control" rows="8" value={text} onChange={onchangedhandler}></textarea>
-            <button className="btn btn-primary" onClick={convertupperhandler}>Convert to UpperCase</button>
+            <div className="mb-3">
+            <textarea className="form-control " rows="8" value={text} onChange={onchangedhandler}></textarea>
+            
+            <button className="btn btn-primary mx-1" onClick={convertupperhandler}>Convert to UpperCase</button>
+            <button className="btn btn-primary mx-1" onClick={convertupperhandler}>Convert to LowerCase</button>
+            </div>
+                    <div className="conatiner my-3">
+                    <h2>Your Text Summary</h2>
+                      <p>{text.split(" ").length} words and {text.length} Characters</p>
+            </div>
         </div>
     )
 }
